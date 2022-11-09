d0cd30139df989244f6097d0b74f90352d8e9bab
monysary <sary.mony@gmail.com>
2022-11-09 15:46:35 -0800

    assigned variable to all possible characters to be used in password

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Declare character options to be used in the password
+var letterLowChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var letterUpChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numChar = [0,1,2,3,4,5,6,7,8,9];
+var specChar = [" ","!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];  
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
