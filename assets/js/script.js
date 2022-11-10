85baf3a83857986f981b403bcbedfa168f1cd3eb
monysary <sary.mony@gmail.com>
2022-11-09 16:15:32 -0800

    Added an empty array called passCharType for use when randomizing what characters to use in the password

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,21 +6,28 @@ var letterLowChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o"
 var letterUpChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numChar = [0,1,2,3,4,5,6,7,8,9];
 var specChar = [" ","!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];  
+var passCharType = []
 
 // Password generation function
 function generatePassword() {
   var passLength = prompt("How many characters do you want in your password?");
 
+  // Checks to see if user input means minimum required password length
   if (passLength < 8) {
     alert("Your password must be at least 8 characters long. Please try again.")
   } else {
     var confLetterLow = confirm("Do you want your password to have lowercase letters?");
+    if (confLetterLow) {passCharType.push(letterLowChar)};
     var confLetterUp = confirm("Do you want your password to have uppercase letters?");
+    if (confLetterUp) {passCharType.push(letterUpChar);}
     var confNumber = confirm("Do you want your password to have numbers?");
+    if (confNumber) {passCharType.push(numChar)};
     var confSpecial = confirm("Do you want your password to have special characters?");
+    if (confSpecial) {passCharType.push(specChar)}
 
   }
 
+  return;
 }
 
 // Write password to the #password input
