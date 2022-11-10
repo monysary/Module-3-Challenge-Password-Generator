d8dd20a7866fd165a702e6a14813f5f5d3daabe8
monysary <sary.mony@gmail.com>
2022-11-10 14:08:38 -0800

    Removed thePassword variable declaration from the global scope cause it's referenced in the local scope of the generatePassword()

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,14 +5,13 @@ var generateBtn = document.querySelector("#generate");
 var letterLowChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var letterUpChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numChar = ["0","1","2","3","4","5","6","7","8","9"];
-var specChar = ["!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];  
-var thePassword = ""
+var specChar = ["!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
 
 // Password generation function
 function generatePassword() {
   var passLength = Number(prompt("How many characters do you want in your password?"));
   var passCharType = []
-  thePassword = ""
+  var thePassword = ""
 
   // Checks to see if user input means minimum and maximum required password length
   if (passLength < 8 || passLength > 128 ) {
