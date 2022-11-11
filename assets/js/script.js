e667e02e311b10116d84a407a1e4a2cf54fb0105
monysary <sary.mony@gmail.com>
2022-11-10 21:30:50 -0800

    Changed password generating logic/for loop to follow a pattern that ensures each character type is used

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,47 +6,55 @@ var letterLowChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o"
 var letterUpChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numChar = ["0","1","2","3","4","5","6","7","8","9"];
 var specChar = ["!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
-var thePassword = ""
+var thePassword = "";
 
 // Password generation function
 function generatePassword() {
   var passLength = Number(prompt("How many characters do you want in your password?"));
-  var passCharType = []
-  thePassword = ""
+  var passCharType = [];
+  var charTypeArr = [];
+  thePassword = "";
 
   // Checks to see if user input a number and meets minimum and maximum required password length
-  if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
+  if (passLength < 8 || passLength > 128 || isNaN(passLength) || passLength === null) {
     alert("Please enter a number. Your password must be at least 8 characters long and no more than 128 characters. Please try again.")
     return;
   } else {
     // Prompts user for the type of characters used in the password
     if (confirm("Do you want your password to have lowercase letters?")) {
-      passCharType = passCharType.concat(letterLowChar)
+      passCharType = passCharType.concat(letterLowChar);
+      charTypeArr.push(letterLowChar);
     };
     if (confirm("Do you want your password to have uppercase letters?")) {
       passCharType = passCharType.concat(letterUpChar);
+      charTypeArr.push(letterUpChar);
     }
     if (confirm("Do you want your password to have numbers?")) {
-      passCharType = passCharType.concat(numChar)
+      passCharType = passCharType.concat(numChar);
+      charTypeArr.push(numChar);
     };
     if (confirm("Do you want your password to have special characters?")) {
-      passCharType = passCharType.concat(specChar)
+      passCharType = passCharType.concat(specChar);
+      charTypeArr.push(specChar);
     };
   };
 
   // Checks if at least one character type was selected
   if (passCharType.length == 0) {
     alert("You must select at least one character type. Please try again.");
-    return;
-  }
-
-  // Used for loop to concatenate characters into thePassword string
-  for (var i = 0; i < passLength; i++) {
-    thePassword = thePassword.concat(passCharType[Math.floor(Math.random() * passCharType.length)])
   };
 
-  // Check if password meets character type criteria
-  
+  // Password generation logic, follows a pattern to ensure each selected character type is used once
+  var loop = charTypeArr.length;
+  loop--;
+  for (var a = 0; a < passLength; a++) {
+    if (loop === -1) {
+      loop = charTypeArr.length;
+      loop--;
+    }
+    thePassword = thePassword.concat(charTypeArr[loop][Math.floor(Math.random() * charTypeArr[loop].length)]);
+    loop--;
+  };
 
   return;
 }
@@ -56,7 +64,6 @@ function writePassword() {
   generatePassword()
   var password = thePassword
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
 
